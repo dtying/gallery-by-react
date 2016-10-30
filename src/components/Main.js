f58664681363fdc5df5a61126e40b7f4aa1e7644
Tingying Ding <Tingying Ding>
2016-10-30 10:54:40 +0800

    init project structure

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <section className="stage">
+        <section className="img-sec"></section>
+        <nav className="controller-nav"></nav>
+      </section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
